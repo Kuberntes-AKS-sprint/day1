167c6b7fb477ffa7ba8b1105454284f49b5b95c0
urtbest86 <urtbest86@gmail.com>
2024-01-24 13:02:27 +0900

    edit msg

--- 1-Challenge/sprintapp.js
+++ 1-Challenge/sprintapp.js
@@ -6,8 +6,10 @@ console.log("Sprint Server starting...");
 var handler = function(request, response) {
     console.log("Received Request from" + request.connection.remoteAddress);
     response.writeHead(200);
-    response.end("새로운 " + os.hostname() + " 생성 되었습니다."+"\n");
+    response.end("Sprint 버전 1 " + os.hostname() + " 생성 되었습니다."+"\n");
 };
 
 var www = http.createServer(handler);
 www.listen(8080);
+
+
