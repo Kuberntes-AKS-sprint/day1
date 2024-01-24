aa7f4fbec064922220097377c431fd324f15f186
urtbest86 <urtbest86@gmail.com>
2024-01-24 12:51:29 +0900

    init

---
+++ 1-Challenge/sprintapp.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const os = require('os');
+
+console.log("Sprint Server starting...");
+
+var handler = function(request, response) {
+    console.log("Received Request from" + request.connection.remoteAddress);
+    response.writeHead(200);
+    response.end("새로운 " + os.hostname() + " 생성 되었습니다."+"\n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
