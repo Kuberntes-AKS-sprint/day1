12cb0e232f01680eff262c5976afae4be04e3043
youjin <56027961+urtbest86@users.noreply.github.com>
2024-01-24 13:07:53 +0900

    Update sprintapp.js

--- 1-Challenge/sprintapp.js
+++ 1-Challenge/sprintapp.js
@@ -6,7 +6,7 @@ console.log("Sprint Server starting...");
 var handler = function(request, response) {
     console.log("Received Request from" + request.connection.remoteAddress);
     response.writeHead(200);
-    response.end("Sprint 버전 1 " + os.hostname() + " 생성 되었습니다."+"\n");
+    response.end("Sprint 버전 1이 실행되었습니다."+"\n");
 };
 
 var www = http.createServer(handler);
